c2f0b35fdd55e3cad689fb97d8e8bf9150f4e695
FrankDonny <arkohfrank1@gmail.com>
2022-10-30 23:46:24 +0000

    0x0E. Web stack debugging #1

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,7 @@
 
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 echo '<h1>hello world, this is a test</h1>' | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
